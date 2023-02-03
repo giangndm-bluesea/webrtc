40c239f8b45777501a37aa91962a7dc4cb9e9a39
Giang Minh <giangndm@ows.vn>
2023-02-03 15:06:46 +0700

    fixing build

--- sdk/objc/native/src/audio/audio_device_ios.mm
+++ sdk/objc/native/src/audio/audio_device_ios.mm
@@ -646,7 +646,7 @@ void AudioDeviceIOS::HandleOutputVolumeChange() {
 }
 
 bool AudioDeviceIOS::RestartAudioUnit(bool enable_input) {
-  RTC_DCHECK_RUN_ON(&thread_);
+  RTC_DCHECK_RUN_ON(thread_);
 
   LOGI() << "RestartAudioUnit";
 
